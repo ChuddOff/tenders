252864b44f87989b1f3bbe4e1f8e1c86d92ef832
LastKnight12 <vovaua27@gmail.com>
2024-08-27 21:22:36 +0300

    navbar rebuild

--- src/app/_components/shared/Header/index.tsx
+++ src/app/_components/shared/Header/index.tsx
@@ -1,29 +1,32 @@
+import Image from "next/image";
 import React from "react";
+import { MdOutlineManageSearch } from "react-icons/md";
+import avatar from "@/images/ava.jpg";
 import { IoMdSearch } from "react-icons/io";
-
-interface HeaderProps {}
 
 const Header = () => {
   return (
-    <header className="flex w-full items-center justify-center gap-[130px]">
+    <header className="mx-auto flex max-w-[1500px] items-center justify-between gap-[130px] px-9">
       <h1 className="text-[42px] font-bold text-main">Tender</h1>
       <div className="flex gap-[12px]">
-        <div className="flex h-[44px] items-center justify-between border-b-[1px] border-main px-[12px] py-[8px]">
+        <div className="flex items-center justify-between border-b-[1px] border-main px-[12px] py-[8px]">
           <input
             type="text"
             placeholder="Поиск по названию"
-            className="h-[43px] cursor-text rounded-[12px] text-[14px] text-[22px] font-normal text-[#000000] focus:border-none focus:outline-none"
+            className="cursor-text rounded-[12px] text-[22px] font-normal text-[#000000] focus:border-none focus:outline-none"
           />
-          <img src="./advancedSearch.svg" alt="" />
+          <MdOutlineManageSearch size={19} className="text-main" />
         </div>
-        <div className="flex h-[44px] w-[45px] items-center justify-center rounded-[100%] bg-main">
+        <div className="flex items-center justify-center rounded-[100%] bg-main">
           <IoMdSearch size={19} className="text-white" />
         </div>
       </div>
-      <img
-        src="./ava.jpg"
-        alt="ava"
-        className="h-[60px] w-[60px] rounded-[100%]"
+      <Image
+        src={avatar}
+        alt="avatar"
+        className="rounded-full"
+        width={19}
+        height={19}
       />
     </header>
   );
